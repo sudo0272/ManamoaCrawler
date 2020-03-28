e2bd92f6a3c9f8e8e2b9a8f94a52d96ca0617aaf
sudo0272 <sudo0272@gmail.com>
2020-03-28 22:09:53 +0900

    add bar support

--- main.py
+++ main.py
@@ -5,10 +5,60 @@ import re
 import os
 import sys
 import zipfile
+import parmap
+import shutil
+import multiprocessing
+import tqdm
 
 MANAMOA_URL = 'https://manamoa.net'
 
 urllib3.disable_warnings()
+
+
+def downloader(i, downloadPath, mangaTitle):
+    os.mkdir('./tmp/%s' % i[0])
+
+    while True:
+        try:
+            images = []
+
+            while True:
+                try:
+                    mangaHtml = http.request('GET', MANAMOA_URL + i[1]).data.decode('utf-8')
+
+                    n = 1
+
+                    with zipfile.ZipFile('%s/%s/%s.cbz' % (downloadPath, mangaTitle, i[0]), 'w') as z:
+                        for j in (j.replace('\\', '') for j in
+                                  re.search(r'(?<=img_list = \[\").*(?=\"\])', mangaHtml).group().split('","')):
+                            fileName = '%d.%s' % (n, j.split('.')[-1])
+                            with open('./tmp/%s/%s' % (i[0], fileName), 'wb') as f:
+                                f.write(http.request('GET', j).data)
+
+                            z.write('./tmp/%s/%s' % (i[0], fileName))
+
+                            n += 1
+
+                        z.close()
+
+                        shutil.rmtree('./tmp/%s' % i[0])
+
+                    break
+
+                except urllib3.exceptions.SSLError:
+                    pass
+
+                except urllib3.exceptions.MaxRetryError:
+                    pass
+
+            break
+
+        except urllib3.exceptions.SSLError:
+            pass
+
+        except urllib3.exceptions.MaxRetryError:
+            pass
+
 
 if __name__ == "__main__":
     http = urllib3.PoolManager()
@@ -19,8 +69,10 @@ if __name__ == "__main__":
     while True:
         word = input('검색어 입력: ')
 
-        mangaListHtml = http.request('GET', MANAMOA_URL + '/bbs/search.php?' + urllib.parse.urlencode(
-            {'stx': word})).data.decode('utf-8')
+        mangaListHtml = http.request('GET', MANAMOA_URL + '/bbs/search.php?' + urllib.parse.urlencode({
+            'stx': word
+        })).data.decode('utf-8')
+
         soup = BeautifulSoup(mangaListHtml, 'html.parser')
 
         mangaCandidates = []
@@ -31,7 +83,7 @@ if __name__ == "__main__":
             continue
 
         for i in mangaSubjectList:
-            mangaCandidates.append((re.search(r'(?<=\d\">)(.|\n)*(?=<\/a>)', i).group().strip(),
+            mangaCandidates.append((re.search(r'(?<=\d\">)(.|\n)*(?=</a>)', i).group().strip(),
                                     re.search(r'(?<=<a href=\").*(?=\">)', i).group().replace('&amp;', '&').strip()))
 
         for i in range(len(mangaCandidates)):
@@ -46,6 +98,7 @@ if __name__ == "__main__":
                 print('다시 입력해주세요')
 
         mangaTitle, mangaListUrl = mangaCandidates[n - 1]
+        mangaTitle = mangaTitle.replace('/', '_')
 
         break
 
@@ -62,52 +115,9 @@ if __name__ == "__main__":
         mangaList.append((re.search(r'.*(?=<span[^>])', i).group().replace(mangaTitle, '').strip(),
                           re.search(r'(?<=<a href=").*(?=">)', i).group().replace('&amp;', '&')))
 
+    if os.path.exists('./tmp/'):
+        shutil.rmtree('./tmp/')
+
     os.mkdir('./tmp/')
 
-    for i in mangaList:
-        while True:
-            try:
-                images = []
-
-                while True:
-                    try:
-                        print('%s html파일 다운로드중...' % (i[0]))
-                        mangaHtml = http.request('GET', MANAMOA_URL + i[1]).data.decode('utf-8')
-
-                        print('%s html파일 파싱중...' % (i[0]))
-                        n = 1
-
-                        with zipfile.ZipFile('%s/%s/%s.cbz' % (downloadPath, mangaTitle, i[0]), 'w') as z:
-                            for j in (j.replace('\\', '') for j in
-                                      re.search(r'(?<=img_list = \[\").*(?=\"\])', mangaHtml).group().split('","')):
-                                fileName = '%d.%s' % (n, j.split('.')[-1])
-                                with open('./tmp/%s' % fileName, 'wb') as f:
-                                    print('%s의 %d번째 파일 다운로드중...' % (i[0], n))
-                                    f.write(http.request('GET', j).data)
-
-                                z.write('./tmp/%s' % fileName)
-
-                                n += 1
-
-                            z.close()
-
-                            for i in os.listdir('./tmp/'):
-                                os.remove('./tmp/%s' % i)
-
-                        break
-
-                    except urllib3.exceptions.SSLError:
-                        pass
-
-                    except urllib3.exceptions.MaxRetryError:
-                        pass
-
-                break
-
-            except urllib3.exceptions.SSLError:
-                pass
-
-            except urllib3.exceptions.MaxRetryError:
-                pass
-
-    # print(mangaList,  end='\n\n\n')
+    parmap.map(downloader, mangaList, downloadPath, mangaTitle, pm_pbar=True, pm_processes=multiprocessing.cpu_count())
